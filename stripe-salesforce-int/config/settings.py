a50eaa43057e4bcfa85a4613d04acd6d7d265947
Rakesh Majhi <150812554+majhiiii@users.noreply.github.com>
2025-06-23 13:38:38 +0530

    Update settings.py

--- stripe-salesforce-int/config/settings.py
+++ stripe-salesforce-int/config/settings.py
@@ -5,7 +5,7 @@ load_dotenv()
 # Salesforce credentials
 SF_CLIENT_ID = os.getenv("SF_CLIENT_ID")
 SF_CLIENT_SECRET = os.getenv("SF_CLIENT_SECRET")
-SF_TOKEN_URL = "https://orgfarm-1cb78eca9b-dev-ed.develop.my.salesforce.com/services/oauth2/token"
+SF_TOKEN_URL = "enter your token url"
 SF_API_VERSION = "v63.0"  # Adjust if needed
 
 # Stripe
